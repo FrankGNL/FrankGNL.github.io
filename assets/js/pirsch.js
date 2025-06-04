4e1839ad14166d33f9a7b07846e006d2ff976cd9
Frank Groenewoud <frank@Larry.local>
2025-06-04 21:53:36 +0200

    code added

---
+++ assets/js/pirsch.js
@@ -0,0 +1,7 @@
+var s = document.createElement('script');
+s.defer = true;
+s.src = 'https://api.pirsch.io/your-pirsch-id/script.js';
+s.id = 'pirschjs';
+s.dataset.code = '7ESGrvguPYaEpA0siNYox28jL5NQnhVt';
+document.head.appendChild(s);
+
